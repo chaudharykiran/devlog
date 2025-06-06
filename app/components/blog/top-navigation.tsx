93cccb912904aeb5aa442f5b686648ea1faaeeb6
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2025-06-06 21:23:58 +0900

    Update writing link to new repo

--- app/components/blog/top-navigation.tsx
+++ app/components/blog/top-navigation.tsx
@@ -31,7 +31,7 @@ export function TopNavigation() {
 
         <div className="flex items-center gap-4">
           <Button className="text-sm rounded-full">
-            <Link to="https://github.com/chaudharykiran/techbro/blob/master/docs/WRITING.md" target="_blank" rel="noopener noreferrer">
+            <Link to="https://github.com/chaudharykiran/devlog/blob/master/docs/WRITING.md" target="_blank" rel="noopener noreferrer">
               Write
             </Link>
           </Button>
